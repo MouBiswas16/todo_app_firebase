d7e38dfff300297078fa693eb18c2683ced91963
Mou Biswas <qtmeaowmou16@gmail.com>
2023-12-20 14:29:46 +0600

    mark task as completed

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:todo_app_firebase/screens/add_task.dart';
 import 'package:todo_app_firebase/screens/description.dart';
@@ -124,7 +125,11 @@ class _HomeScreenState extends State<HomeScreen> {
                                     SizedBox(
                                       child: IconButton(
                                         icon: Icon(Icons.check),
-                                        onPressed: () async {},
+                                        onPressed: () async {
+                                          Fluttertoast.showToast(
+                                            msg: "Task Completed!!!",
+                                          );
+                                        },
                                       ),
                                     ),
                                     SizedBox(width: 10),
